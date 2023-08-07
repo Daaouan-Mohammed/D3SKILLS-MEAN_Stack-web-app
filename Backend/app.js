02641c16edc612ab5c20daad9f4e59e7a709d4ba
Daaouan-Mohammed <mohammeddaaouan2018@gmail.com>
2023-08-07 19:27:34 +0100

    connection to mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://mohammed:AbCd2016@cluster0.rpygsuh.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 app.use((req, res) => {
-   res.json({ message: 'Votre requête use a bien été reçue !' }); 
+   res.json({ message: 'Votre requête use a bien été reçue avec connection to mongodb!' }); 
 });
 
 module.exports = app;
